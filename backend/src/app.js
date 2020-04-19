34a49b73667e273e9d645fad85e6591f481a2884
Laura Kimberly <lauraksp@icloud.com>
2020-04-19 14:14:35 -0300

    backend da aplicação

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const express = require ('express');
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+module.exports = app;
